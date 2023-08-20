e43c862b0ca4902622c6d584c45b3ff6603f805a
Jack Richings <jack.richings21@gmail.com>
2023-08-20 17:02:26 +0100

    roster

--- app.py
+++ app.py
@@ -1,15 +1,27 @@
 import numpy as np
+import os
 import pandas as pd
 import streamlit as st
 
-
 st.set_page_config(initial_sidebar_state='collapsed')
 
-
 @st.cache_data
+class Roster:
+    def __init__(self, nmax=500):
+        self.columns = ['Name', 'Age', 'Gender', 'Ancestry',
+                        'Profession', 'Alignment', 'Drawback',
+                        'Disorder', 'Companion', 'Retainer']
+        self.df = pd.DataFrame(columns=self.columns, index=range(nmax))
+        self.count = 0
+        
+    def add_character(self, character_dict):
+        for attr in self.columns[1:]:
+            if character_dict[attr] is not None:
+                self.df[attr][self.count] = character_dict[attr]
+        self.count += 1
+
 def convert_df(df):
     return df.to_csv(index=False).encode('utf-8')
-
 
 
 def convert_rarity_to_probability(df, weight_dict):
@@ -25,126 +37,133 @@ def draw_sample(df, nmax=1, bias=8):
     probs = np.ones(nmax) / (bias**np.arange(nmax))
     probs /= probs.sum()
     num_choices = 1+np.random.choice(nmax, p=probs)
-    idx_choices = np.random.choice(len(df), p=df['Probability'], size=num_choices, replace=False)
-    choice_probs = df['Probability'][idx_choices]
-    choices = df[col0][idx_choices[np.argsort(choice_probs)]].values
-    if nmax==1:
+    idx_choices = np.random.choice(len(df), p=df['Probability'],
+                                   size=num_choices, replace=False)
+    choice_probs = df['Probability'][idx_choices].values
+    choices = df[col0][idx_choices].values
+    choices = choices[np.argsort(choice_probs)]
+    if len(choices)==1:
         return choices[0]
     else:
-        return choices
-           
+        return list(choices)
+    
 
-def format_results(age, gender, ancestry, profession_list, alignment,
-                   drawback, has_drawback, disorder, has_disorder,
-                   companion, has_companion, retainer, has_retainer):
-    agestr = age.lower()
-    gstr = gender.lower()
-    astr = ancestry.lower()
-    if len(profession_list)==1:
-        pstr = profession_list[0].lower()
-    else:
-        pstr = profession_list[0].lower() + ' (former %s)'%profession_list[1].lower()
-    asplit = tuple(alignment.split('-'))
-    xstr = 'They exhibit %s, sometimes manifesting as %s.'%asplit
-    if has_drawback:
-        xstr += ' They %s.'%drawback
-    if has_disorder:
-        xstr += ' They %s.'%disorder
-    if has_companion:
-        xstr +=' They have a %s companion.'%companion.lower()
-    if has_retainer:
-        xstr +=' They have a %s retainer.'%retainer.lower()
-    if agestr.startswith('a') or agestr.startswith('e'):
-        return 'An %s %s %s %s. %s'%(agestr, gstr, astr, pstr, xstr)
-    else:
-        return 'A %s %s %s %s. %s'%(agestr, gstr, astr, pstr, xstr)
+def create_character(df_list, optional_weights):
+    character_dict = {}
+    for df in df_list:
+        attribute = df.columns[0]
+        if attribute=='Profession':
+            sample = draw_sample(df, nmax=2, bias=4)
+            if type(sample)==list:
+                sample = '%s (formerly %s)'%(sample[0], sample[1])
+        elif attribute in optional_weights.keys():
+            prob = optional_weights[attribute]
+            if np.random.random_sample()<prob:
+                sample = draw_sample(df)
+            else:
+                sample = None
+        else:
+            sample = draw_sample(df)
+            if attribute=='Ancestry' and sample=='genasi':
+                sample = 'genasi (%s)'%draw_sample(df, nmax=1)
+        character_dict[attribute] = sample
+    return character_dict
 
+                
+def generate_character_description(character_dict):
+    age = character_dict['Age']
+    ancestry = character_dict['Ancestry']
+    profession = character_dict['Profession']
+    alignment = character_dict['Alignment']
+    drawback = character_dict['Drawback']
+    disorder = character_dict['Disorder']
+    companion = character_dict['Companion']
+    retainer = character_dict['Retainer']
+    output = 'They are a %s %s %s.  \n' %(age, ancestry, profession)
+    if age.startswith('a') or age.startswith('e'):
+        output = output.replace(' a ', ' an ')
+    virtue, vice = alignment.split('-')
+    output += 'They try to embody %s, but can lapse into %s.  \n'%(virtue, vice)
+    for extra in [drawback, disorder]:
+        if extra is not None:
+            output += 'They %s. '%extra
+    for extra in [companion, retainer]:
+        if extra is not None:
+            output += 'They have a %s companion.'%extra.lower()
+            break
+    return output.strip()
 
+def add_character_to_roster(character_dict):
+    df_character = pd.DataFrame(character_dict, index=[0])
+    st.session_state.character_list.append(character_dict)
+    st.session_state.df = pd.concat([st.session_state.df, df_character], ignore_index=True)
+    
 
+def delete_roster():
+    columns = ['Name', 'Age', 'Gender', 'Ancestry',
+               'Profession', 'Alignment', 'Drawback',
+               'Disorder', 'Companion', 'Retainer']
+    st.session_state.character_list = []
+    st.session_state.df = pd.DataFrame(columns=columns)
+
+    
+    
 st.title('Welcome to Jack\'s character generator')
 
+df_weirdness = pd.read_csv('weirdness_weights.csv')
+rarities = df_weirdness[df_weirdness.columns[0]]
+weirdness_prompt = 'Select the level of weirdness for your characters'
+weirdness = st.radio(weirdness_prompt, df_weirdness.columns[1:], horizontal=True, index=1)
+weights = df_weirdness[weirdness]
+weight_dict = dict(zip(rarities, weights))
 
-df_prof = pd.read_csv('./Data/professions.csv')
-df_ancestry = pd.read_csv('./Data/ancestries.csv')
-df_gender = pd.read_csv('./Data/genders.csv')
-df_align = pd.read_csv('./Data/alignments.csv')
-df_db = pd.read_csv('./Data/drawbacks.csv')
-df_dis = pd.read_csv('./Data/disorders.csv')
-df_age = pd.read_csv('./Data/ages.csv')
-df_companions = pd.read_csv('./Data/companions.csv')
-df_retainers = pd.read_csv('./Data/retainers.csv')
-
-wd = {'none': 0,
-      'common': 12,
-      'uncommon': 4,
-      'rare': 2,
-      'very rare': 1}
+d0 = './Data'
+flist_csv = [os.path.join(d0, f) for f in os.listdir(d0) if f.endswith('.csv')]
+df_list = []
+for f_csv in flist_csv:
+    df = pd.read_csv(f_csv)
+    convert_rarity_to_probability(df, weight_dict)
+    df_list.append(df)
     
 with st.sidebar:
     st.write('Adjust weightings for different rarities')
-    w_common = st.slider('Common', 1, 100, wd['common'])
-    w_uncommon = st.slider('Uncommon', 1, w_common, wd['uncommon'])
-    w_rare = st.slider('Rare', 1, w_uncommon, wd['rare'])
-    w_vrare = st.slider('Very rare', 1, w_rare, wd['very rare'])
-    wd['common'] = w_common
-    wd['uncommon'] = w_uncommon
-    wd['rare'] = w_rare
-    wd['very rare'] = w_vrare
-    p_db = st.slider('Probability of having a drawback', 0.0, 1.0, 0.5)
-    p_dis = st.slider('Probability of having a disorder', 0.0, 1.0, 0.15)
-    p_comp = st.slider('Probability of having a companion', 0.0, 1.0, 0.2)
-    p_ret = st.slider('Probability of having a retainer', 0.0, 1.0, 0.1)
-    
-for df in [df_ancestry, df_prof, df_gender, df_align, df_db, 
-           df_dis, df_age, df_companions, df_retainers]:
-    convert_rarity_to_probability(df, wd)
-    
-    
-num_char = st.slider('How many characters would you like to generate?', 1, 20, 1)
-columns = ['Name', 'Age', 'Gender', 'Ancestry', 'Profession', 'Former Profession',
-           'Alignment', 'Drawback', 'Disorder', 'Companion', 'Retainer']
-df_out = pd.DataFrame(columns=columns, index=range(num_char))
-if st.button('Generate!'):
-    for i in range(num_char):
-        age = draw_sample(df_age, nmax=1)
-        gender = draw_sample(df_gender, nmax=1)
-        ancestry = draw_sample(df_ancestry, nmax=1)
-        if ancestry=='genasi':
-            ancestry = 'genasi (%s)'%draw_sample(df_ancestry, nmax=1)
-        profession_list = draw_sample(df_prof, nmax=2, bias=4)
-        alignment = draw_sample(df_align, nmax=1)
-        drawback = draw_sample(df_db, nmax=1)
-        disorder = draw_sample(df_dis, nmax=1)
-        companion = draw_sample(df_companions, nmax=1)
-        retainer = draw_sample(df_retainers, nmax=1)
-        samp1, samp2, samp3, samp4 = np.random.rand(4)
-        description = format_results(age, gender, ancestry, profession_list, alignment,
-                                     drawback, p_db>samp1, disorder, p_dis>samp2,
-                                     companion, p_comp>samp3, retainer, p_ret>samp4)
-        st.write(description)
-        df_out['Age'][i] = age
-        df_out['Gender'][i] = gender
-        df_out['Ancestry'][i] = ancestry
-        df_out['Profession'][i] = profession_list[0]
-        if len(profession_list)>1:
-            df_out['Former Profession'][i] = profession_list[1]
-        df_out['Alignment'][i] = alignment
-        if p_db>samp1:
-            df_out['Drawback'][i] = drawback
-        if p_dis>samp2:
-            df_out['Disorder'][i] = disorder
-        if p_comp>samp3:
-            df_out['Companion'][i] = companion
-        if p_ret>samp4:
-            df_out['Retainer'][i] = retainer
-            
-    csv = convert_df(df_out)
+    p_db = st.slider('Probability of generating a drawback', 0.0, 1.0, 0.5)
+    p_dis = st.slider('Probability of generating a disorder', 0.0, 1.0, 0.15)
+    p_comp = st.slider('Probability of generating a companion', 0.0, 1.0, 0.1)
+    p_ret = st.slider('Probability of generating a retainer', 0.0, 1.0, 0.0)
+    option_weights = {'Drawback': p_db,
+                      'Disorder': p_dis,
+                      'Companion': p_comp,
+                      'Retainer': p_ret}
 
-    st.download_button(
-       "Press to Download",
-       csv,
-       "my_random_characters.csv",
-       "text/csv",
-       key='download-csv'
-    )
+    
+num_prompt = 'How many characters would you like to generate?'
+
+columns = ['Name', 'Age', 'Gender', 'Ancestry',
+           'Profession', 'Alignment', 'Drawback',
+           'Disorder', 'Companion', 'Retainer']
+if 'character_list' not in st.session_state:
+    st.session_state.character_list = []
+    st.session_state.df = pd.DataFrame(columns=columns)
+    
+
+
+if st.button('Generate!'):
         
+    character_dict = create_character(df_list, option_weights)
+    description = generate_character_description(character_dict)
+    st.write(description)
+    st.button('Add character to roster', on_click=add_character_to_roster, args=(character_dict,))
+
+
+with st.expander('Show roster'):
+    csv = convert_df(st.session_state.df)
+    dl_prompt = 'Press to download characters in csv format'
+    st.download_button(dl_prompt, csv, 'my_random_characters.csv', 'text/csv')
+    for character_dict in st.session_state.character_list:
+        description = generate_character_description(character_dict)
+        st.write(description)
+    st.button('Delete roster?', on_click=delete_roster)
+    
+    
+    
